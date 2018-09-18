e5ccd59fc9efa0a6ac6dc28d00307240bbf1d99a
Mazawrath <Mazawrath@users.noreply.github.com>
2018-09-18 13:46:02 -0400

    Added .blessed

---
+++ src/main/java/com/mazawrath/beanbot/commands/copypasta/BlessedCommand.java
@@ -0,0 +1,38 @@
+package com.mazawrath.beanbot.commands.copypasta;
+
+import com.mazawrath.beanbot.utilities.Points;
+import de.btobastian.sdcf4j.Command;
+import de.btobastian.sdcf4j.CommandExecutor;
+import org.javacord.api.entity.channel.ServerTextChannel;
+import org.javacord.api.entity.message.MessageBuilder;
+import org.javacord.api.entity.server.Server;
+import org.javacord.api.entity.user.User;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class BlessedCommand implements CommandExecutor {
+    private Points points;
+
+    public BlessedCommand(Points points) {
+        this.points = points;
+    }
+
+    @Command(
+            aliases = {"blessed"},
+            privateMessages = false
+    )
+
+    public void onCommand(ServerTextChannel serverTextChannel, User author, Server server) {
+        if (points.removePoints(author.getIdAsString(), server.getIdAsString(), 2)) {
+            try {
+                new MessageBuilder()
+                        .addAttachment(new URL("https://cdn.discordapp.com/attachments/480959729330290688/489855609450135562/unknown.png"))
+                        .send(serverTextChannel);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        } else
+            serverTextChannel.sendMessage("You do not have enough beanCoin for this command");
+    }
+}
