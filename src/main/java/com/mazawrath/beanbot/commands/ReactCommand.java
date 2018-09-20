899c8622f61bec51146bf26ec4ba916b9590cab2
Mazawrath <Mazawrath@users.noreply.github.com>
2018-09-20 12:52:33 -0400

    Attempting to fix .react. Does not currently work.

--- src/main/java/com/mazawrath/beanbot/commands/ReactCommand.java
+++ src/main/java/com/mazawrath/beanbot/commands/ReactCommand.java
@@ -28,11 +28,14 @@ public class ReactCommand implements CommandExecutor {
             if (points.removePoints(author.getIdAsString(), server.getIdAsString(), 2)) {
                 Message messsageBefore = message.getMessagesBefore(1).join().first();
                 message.delete();
-                for (int i = 0; i < argument.length(); i++) {
-                    if (StringUtils.isAlpha(String.valueOf(argument.toLowerCase().charAt(i))))
-                        messsageBefore.addReaction(EmojiParser.parseToUnicode(":regional_indicator_symbol_" + argument.toLowerCase().charAt(i) + ":"));
-                    else if (StringUtils.isAlphaSpace(String.valueOf(argument.toLowerCase().charAt(i))))
-                        messsageBefore.addReaction(EmojiParser.parseToUnicode(":large_blue_circle:"));
+                for (int i = 0; i < command.length; i++) {
+                    for (int j = 0; i < command[i].length(); j++) {
+                        if (StringUtils.isAlpha(String.valueOf(command[i].toLowerCase().charAt(j))))
+                            messsageBefore.addReaction(EmojiParser.parseToUnicode(":regional_indicator_symbol_" + command[i].toLowerCase().charAt(j) + ":"));
+//                        else if (StringUtils.isAlphaSpace(String.valueOf(command[i].toLowerCase().charAt(j))))
+//                            messsageBefore.addReaction(EmojiParser.parseToUnicode(":large_blue_circle:"));
+                    }
+                    messsageBefore.addReaction(EmojiParser.parseToUnicode(":large_blue_circle:"));
                 }
             } else
                 serverTextChannel.sendMessage("You do not have enough beanCoin to use this command.");
