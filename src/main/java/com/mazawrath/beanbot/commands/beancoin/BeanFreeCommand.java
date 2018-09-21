ee4d0ed6c88f89298780d39a8d33f0507853d4a9
Mazawrath <Mazawrath@users.noreply.github.com>
2018-09-20 22:49:22 -0400

    Fixed spacing on BeanFreeCommand

--- src/main/java/com/mazawrath/beanbot/commands/beancoin/BeanFreeCommand.java
+++ src/main/java/com/mazawrath/beanbot/commands/beancoin/BeanFreeCommand.java
@@ -7,7 +7,7 @@ import org.javacord.api.entity.channel.ServerTextChannel;
 import org.javacord.api.entity.server.Server;
 import org.javacord.api.entity.user.User;
 
-public class BeanFreeCommand implements CommandExecutor{
+public class BeanFreeCommand implements CommandExecutor {
     private Points points;
 
     public BeanFreeCommand(Points points) {
