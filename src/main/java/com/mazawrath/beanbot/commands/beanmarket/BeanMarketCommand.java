851695bd56552355ab132587320155b1b944ce26
Mazawrath <Mazawrath@users.noreply.github.com>
2018-09-21 23:36:51 -0400

    Fixed description of BeanMarketCommand

--- src/main/java/com/mazawrath/beanbot/commands/beanmarket/BeanMarketCommand.java
+++ src/main/java/com/mazawrath/beanbot/commands/beanmarket/BeanMarketCommand.java
@@ -17,7 +17,7 @@ public class BeanMarketCommand implements CommandExecutor {
     @Command(
             aliases = {"beanmarket"},
             description = "Gets a status of the Bean Market. Leave argument blank for a list of every company",
-            usage = "beanbalance",
+            usage = "beanmarket",
             privateMessages = false
     )
 
