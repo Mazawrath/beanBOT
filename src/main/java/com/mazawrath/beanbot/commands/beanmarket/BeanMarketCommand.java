c9992828fe02b6ced577960871be37fbd31c78ae
Mazawrath <Mazawrath@users.noreply.github.com>
2018-09-21 21:57:33 -0400

    Temporally changing to show that investing is WIP

--- src/main/java/com/mazawrath/beanbot/commands/beanmarket/BeanMarketCommand.java
+++ src/main/java/com/mazawrath/beanbot/commands/beanmarket/BeanMarketCommand.java
@@ -39,7 +39,8 @@ public class BeanMarketCommand implements CommandExecutor {
                     .setTitle("Bean Market")
                     .addInlineField("Symbol", symbol[0])
                     .addInlineField("Price", price[0])
-                    .setFooter("Use .beanmarket [Symbol] to look up a sepcific company. Use .beaninvest to check your portfolio.");
+                    .setFooter("Investing coming soon!");
+                    //.setFooter("Use .beanmarket [Symbol] to look up a sepcific company. Use .beaninvest to check your portfolio.");
             serverTextChannel.sendMessage(embed);
         } else {
             if (Arrays.asList(companies).contains(args[0])) {
