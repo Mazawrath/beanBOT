42af50c82ee705dc39caf92c0db5656c585ee716
Mazawrath <Mazawrath@users.noreply.github.com>
2018-10-18 19:29:23 -0400

    Removed unnecessary imports

--- src/main/java/com/mazawrath/beanbot/commands/beanmarket/BeanMarketCommand.java
+++ src/main/java/com/mazawrath/beanbot/commands/beanmarket/BeanMarketCommand.java
@@ -6,8 +6,6 @@ import de.btobastian.sdcf4j.Command;
 import de.btobastian.sdcf4j.CommandExecutor;
 import org.javacord.api.entity.channel.ServerTextChannel;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
-import org.javacord.api.entity.server.Server;
-import org.javacord.api.entity.user.User;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
