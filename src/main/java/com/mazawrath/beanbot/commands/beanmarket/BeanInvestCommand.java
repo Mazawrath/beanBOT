50a41b5dd0bce568c1907e2cef109b3e4cd5430d
Mazawrath <Mazawrath@users.noreply.github.com>
2018-10-19 23:49:50 -0400

    Attempted to fix bean market investing

--- src/main/java/com/mazawrath/beanbot/commands/beanmarket/BeanInvestCommand.java
+++ src/main/java/com/mazawrath/beanbot/commands/beanmarket/BeanInvestCommand.java
@@ -36,14 +36,17 @@ public class BeanInvestCommand implements CommandExecutor {
             if (args.length >= 3) {
                 if (stockMarket.isProperDecimal(args[2])) {
                     if (StockMarket.getSymbol(args[1].toUpperCase()) != null) {
-                        if (points.removePoints(author.getIdAsString(), null, server.getIdAsString(), new BigDecimal(args[2]).setScale(Points.SCALE, Points.ROUNDING_MODE))) {
-                            BigDecimal sharesBought = stockMarket.buyShares(author.getIdAsString(), server.getIdAsString(), args[1].toUpperCase(), new BigDecimal(args[2]).setScale(Points.SCALE, Points.ROUNDING_MODE));
-                            if (sharesBought.compareTo(BigDecimal.ZERO) > 0)
-                                serverTextChannel.sendMessage("Bought " + sharesBought + " shares from " + stockMarket.getComapanyName(args[1].toUpperCase()));
-                            else
-                                serverTextChannel.sendMessage("some error happened and I don't know what it is.");
+                        if (new BigDecimal(args[2]).divide(stockMarket.getStockPrice(StockMarket.getSymbol(args[1].toUpperCase()), true)).setScale(Points.SCALE, Points.ROUNDING_MODE).compareTo(BigDecimal.ZERO) < .01) {
+                            if (points.removePoints(author.getIdAsString(), null, server.getIdAsString(), new BigDecimal(args[2]).setScale(Points.SCALE, Points.ROUNDING_MODE))) {
+                                BigDecimal sharesBought = stockMarket.buyShares(author.getIdAsString(), server.getIdAsString(), args[1].toUpperCase(), new BigDecimal(args[2]).setScale(Points.SCALE, Points.ROUNDING_MODE));
+                                if (sharesBought.compareTo(BigDecimal.ZERO) > 0)
+                                    serverTextChannel.sendMessage("Bought " + sharesBought + " shares from " + stockMarket.getComapanyName(args[1].toUpperCase()));
+                                else
+                                    serverTextChannel.sendMessage("some error happened and I don't know what it is.");
+                            } else
+                                serverTextChannel.sendMessage("You do not have enough beanCoin for this command");
                         } else
-                            serverTextChannel.sendMessage("You do not have enough beanCoin for this command");
+                            serverTextChannel.sendMessage("You must buy at least 1% of the share.");
                     } else
                         serverTextChannel.sendMessage("Symbol not found.");
                 } else
