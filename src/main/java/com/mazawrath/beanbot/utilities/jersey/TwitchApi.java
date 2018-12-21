8d5e3cfc598e190d73a001dccfd02d88b982263b
Mazawrath <Mazawrath@users.noreply.github.com>
2018-12-21 01:40:59 -0500

    Added encrypter method

--- src/main/java/com/mazawrath/beanbot/utilities/jersey/TwitchApi.java
+++ src/main/java/com/mazawrath/beanbot/utilities/jersey/TwitchApi.java
@@ -1,10 +1,15 @@
 package com.mazawrath.beanbot.utilities.jersey;
 
+import com.google.api.client.util.Base64;
 import org.json.JSONObject;
 
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.net.URL;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 @Path("/twitchapi")
 public class TwitchApi {
@@ -19,11 +24,30 @@ public class TwitchApi {
 
     @POST
     @Path("/subscription")
-    public void response(@HeaderParam("x-hub-signature") String signature, @HeaderParam("content-length") int length, JSONObject response) {
+    public void response(@HeaderParam("x-hub-signature") String signature, @HeaderParam("content-length") int length, String payload) {
         // TODO handle livestream notification
+        sha256Encrypter(payload, signature);
         System.out.println("Someone went live");
-        System.out.println(response);
+        System.out.println(payload);
         System.out.println(length);
         System.out.println(signature);
     }
+
+
+    private String sha256Encrypter(String payload, String secret) {
+        try {
+            Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+            SecretKeySpec secret_key = new SecretKeySpec(secret.getBytes(), "HmacSHA256");
+            sha256_HMAC.init(secret_key);
+
+            String hash = Base64.encodeBase64String(sha256_HMAC.doFinal(payload.getBytes()));
+            System.out.println(hash);
+            return hash;
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 }
