f288543b7ecf475abe2c1fb886fb23d8ca4ab7d7
Mazawrath <Mazawrath@users.noreply.github.com>
2019-02-22 22:53:39 -0500

    Updated method names and added possible new options

--- src/main/java/com/mazawrath/beanbot/utilities/photo/MarvinRequest.java
+++ src/main/java/com/mazawrath/beanbot/utilities/photo/MarvinRequest.java
@@ -14,18 +14,18 @@ import java.net.URL;
 import java.net.URLConnection;
 
 public class MarvinRequest {
-    URL url2;
     BufferedImage buffImage;
     MarvinImage image;
 
     public MarvinRequest(URL url) throws IOException {
-        url2 = url;
         buffImage = ImageIO.read(new ByteArrayInputStream(downloadFile(url)));
         image = new MarvinImage(buffImage);
     }
 
-    public File deepFryImage() throws IOException {
-        MarvinPluginCollection.brightnessAndContrast(image, 10, 140);
+    public File getDeepFry() throws IOException {
+        int pixelValue = image.getHeight() * image.getWidth() ;
+        MarvinPluginCollection.pixelize(image, image, 1);
+        MarvinPluginCollection.brightnessAndContrast(image, 0, 140);
         MarvinPluginCollection.colorChannel(image, 100, 10, 10);
         image.update();
 
@@ -34,7 +34,7 @@ public class MarvinRequest {
         return output;
     }
 
-    public File embossImage() throws IOException {
+    public File getEmboss() throws IOException {
         MarvinPluginCollection.emboss(image, image);
         image.update();
 
@@ -43,8 +43,17 @@ public class MarvinRequest {
         return output;
     }
 
-    public File invertImage() throws IOException {
+    public File getInvert() throws IOException {
         MarvinPluginCollection.invertColors(image);
+        image.update();
+
+        File output = new File("Output.png");
+        ImageIO.write(image.getBufferedImage(), "png", output);
+        return output;
+    }
+
+    public File getEdges() throws IOException {
+        MarvinPluginCollection.sobel(image,image);
         image.update();
 
         File output = new File("Output.png");
